a81f73b0f8f941204a72c1341e1d9edc6787e224
Dahye Lim <rtyu9874@gmail.com>
2024-12-23 16:41:35 +0900

    엔티티 개발 - 연관관계 있음

--- src/main/kotlin/com/dahye/portfolio/domain/repository/ProjectSkillRepository.kt
+++ src/main/kotlin/com/dahye/portfolio/domain/repository/ProjectSkillRepository.kt
@@ -1,6 +1,6 @@
 package com.dahye.portfolio.domain.repository
 
-import com.dahye.portfolio.domain.entity.Projectskill
+import com.dahye.portfolio.domain.entity.ProjectSkill
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface ProjectSkillRepository : JpaRepository<Projectskill, Long>
+interface ProjectSkillRepository : JpaRepository<ProjectSkill, Long>
