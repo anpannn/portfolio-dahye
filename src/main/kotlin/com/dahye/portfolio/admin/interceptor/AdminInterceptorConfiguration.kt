9b1df5e59607ea2e00488b147403fdeadba46f5c
Dahye Lim <rtyu9874@gmail.com>
2024-12-25 14:57:14 +0900

    공통 개발 - 인터셉터

--- src/main/kotlin/com/dahye/portfolio/admin/interceptor/AdminInterceptorConfiguration.kt
+++ src/main/kotlin/com/dahye/portfolio/admin/interceptor/AdminInterceptorConfiguration.kt
@@ -6,9 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class AdminInterceptorConfigurationval(
-    adminInterceptor: AdminInterceptor
+    val adminInterceptor: AdminInterceptor
 ) : WebMvcConfigurer {
 
     override fun addInterceptors(registry: InterceptorRegistry) {
+
+        registry.addInterceptor(adminInterceptor)
+            .addPathPatterns("/admin/**")
+            .excludePathPatterns("/as sets/**", "/css/**", "/js/**", "h2**")
     }
 }
