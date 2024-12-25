12aea996491302949db5dc283e4e6d9a45bf5a29
Dahye Lim <rtyu9874@gmail.com>
2024-12-25 14:36:48 +0900

    공통 개발 - Exception과 Advice

--- src/main/kotlin/com/dahye/portfolio/admin/exception/AdminException.kt
+++ src/main/kotlin/com/dahye/portfolio/admin/exception/AdminException.kt
@@ -7,3 +7,11 @@ abstract class AdminException(httpStatus: HttpStatus, message: String) : Runtime
     val httpStatus: HttpStatus = httpStatus
 
 }
+
+class AdminBadRequestException(message: String) :
+    AdminException(HttpStatus.BAD_REQUEST, message)
+
+
+
+class AdminInternalServerErrorException(message: String) :
+    AdminException(HttpStatus.INTERNAL_SERVER_ERROR, message)
