0ec465412277376ce6c1f8b52b20fe4e6fbd2e41
Dahye Lim <rtyu9874@gmail.com>
2024-12-26 09:48:11 +0900

    삽입, 수정 API 개발 - 연관관계 없음

--- src/main/kotlin/com/dahye/portfolio/admin/context/introduction/service/AdminIntroductionService.kt
+++ src/main/kotlin/com/dahye/portfolio/admin/context/introduction/service/AdminIntroductionService.kt
@@ -1,9 +1,11 @@
 package com.dahye.portfolio.admin.context.introduction.service
 
+import com.dahye.portfolio.admin.context.introduction.form.IntroductionForm
 import com.dahye.portfolio.admin.data.TableDTO
 import com.dahye.portfolio.domain.entity.Introduction
 import com.dahye.portfolio.domain.repository.IntroductionRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class AdminIntroductionService(private val introductionRepository: IntroductionRepository) {
@@ -13,5 +15,17 @@ class AdminIntroductionService(private val introductionRepository: IntroductionR
         val entities = introductionRepository.findAll()
 
         return TableDTO.from(classInfo, entities)
+    }
+
+    @Transactional
+    fun save(form: IntroductionForm) {
+        val introduction = form.toEntity()
+        introductionRepository.save(introduction)
+    }
+
+    @Transactional
+    fun update(id: Long, form: IntroductionForm) {
+        val introduction = form.toEntity(id)
+        introductionRepository.save(introduction)
     }
 }
--- src/main/kotlin/com/dahye/portfolio/admin/context/link/service/AdminLinkService.kt
+++ src/main/kotlin/com/dahye/portfolio/admin/context/link/service/AdminLinkService.kt
@@ -1,9 +1,11 @@
 package com.dahye.portfolio.admin.context.link.service
 
+import com.dahye.portfolio.admin.context.link.form.LinkForm
 import com.dahye.portfolio.admin.data.TableDTO
 import com.dahye.portfolio.domain.entity.Link
 import com.dahye.portfolio.domain.repository.LinkRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class AdminLinkService(private val linkRepository: LinkRepository) {
@@ -13,5 +15,17 @@ class AdminLinkService(private val linkRepository: LinkRepository) {
         val entities = linkRepository.findAll()
 
         return TableDTO.from(classInfo, entities)
+    }
+
+    @Transactional
+    fun save(form: LinkForm) {
+        val link = form.toEntity()
+        linkRepository.save(link)
+    }
+
+    @Transactional
+    fun update(id: Long, form: LinkForm) {
+        val link = form.toEntity(id)
+        linkRepository.save(link)
     }
 }
